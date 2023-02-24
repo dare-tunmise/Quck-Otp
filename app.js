1ae48430f8dcdf5040660c6dbcabf63185872380
Dare Tunmise <daretunmise@yahoo.com>
2023-02-24 23:12:12 +0100

    committing quick otp

---
+++ app.js
@@ -0,0 +1,19 @@
+let otpDigits = [1, 2, 3,4,5,6,7,8,9];
+
+let button = document.getElementById("btn");
+
+const otp = document.querySelector(".otp");
+
+button.addEventListener("click", ()=>{
+    let newOtp = " ";
+    for (let i = 0; i < 6; i++ ) {
+        
+        newOtp += otpDigits[generateNumber()];
+
+        otp.textContent = newOtp;
+    }
+})
+
+function generateNumber(){
+   return Math.floor(Math.random() * otpDigits.length)
+}
