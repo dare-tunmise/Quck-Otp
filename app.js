3829962719ceee35842d6e12193cf8db34e0c64a
Dare Tunmise <84666598+dare-tunmise@users.noreply.github.com>
2023-02-26 21:25:09 +0100

    Update app.js
    A simple update on QuickOTP

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ let button = document.getElementById("btn");
 
 const otp = document.querySelector(".otp");
 
-button.addEventListener("click", ()=>{
-    let newOtp = " ";
+button.addEventListener("click", generateOtp)
+
+function generatOtp () {
+
+
+let newOtp = " ";
     for (let i = 0; i < 6; i++ ) {
         
         newOtp += otpDigits[generateNumber()];
 
         otp.textContent = newOtp;
     }
-})
+
+
+
+}
+
 
 function generateNumber(){
    return Math.floor(Math.random() * otpDigits.length)
